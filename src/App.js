1e49e8e2f8a140b60df30137d0a1a0aa2f9a90ea
[Lisa Miner] <midnightmoet@gmail.com>
2021-10-24 04:00:47 -0400

    Adding webcam feature first

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="app">
       <h1>Lets Build Snapchat</h1>
+      <WebcamCapture />
     </div>
   );
 }
---
+++ src/WebcamCapture.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+const videoConstraints = {
+    width: 250,
+    height: 400,
+    facingMode: "user",
+};
+
+
+function WebcamCapture() {
+    return (
+        <div className="webcamCapture">
+            <Webcam 
+                
+            />
+        </div>
+    )
+}
+
+export default WebcamCapture
