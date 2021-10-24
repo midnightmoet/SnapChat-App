5d099dc9b2c2f32eccd53d9a10d82849c4289396
[Lisa Miner] <midnightmoet@gmail.com>
2021-10-24 05:03:09 -0400

    adding button for webcam

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
     <div className="app">
       <h1>Lets Build Snapchat</h1>
-      <WebcamCapture />
+      <WebcamCapture/>
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import Webcam from 'react-webcam';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 const videoConstraints = {
     width: 250,
@@ -9,13 +10,29 @@ const videoConstraints = {
 
 
 function WebcamCapture() {
+    const webcamRef = useRef(null);
+
+    const capture =  useCallback(() => {
+
+    }, [webcamRef]);
+
     return (
         <div className="webcamCapture">
             <Webcam 
-                
+                audio={false}
+                height={videoConstraints.height}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={videoConstraints.width}
+                videoConstraints={videoConstraints}
+            />
+
+            <RadioButtonUncheckedIcon
+                className="webcamCapture__button"
+                onClick={capture}
             />
         </div>
     )
 }
 
-export default WebcamCapture
+export default WebcamCapture;
