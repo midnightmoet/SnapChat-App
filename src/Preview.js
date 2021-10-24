4039450ee2ed71acea7e3179adf65a5dc2a3f7be
[Lisa Miner] <midnightmoet@gmail.com>
2021-10-24 14:46:27 -0400

    more code, css, stuff added

--- src/Preview.js
+++ src/Preview.js
@@ -4,6 +4,13 @@ import { useHistory } from 'react-router';
 import { selectCameraImage, resetCameraImage } from './features/cameraSlice';
 import './Preview.css';
 import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
@@ -23,6 +30,16 @@ function Preview() {
     return (
         <div className="preview">
             <CloseIcon onClick={closePreview} className="preview-close"/>
+            <div className="preview-toolbarRight">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />   
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            
+            </div>
             <img src={cameraImage} alt="preview" />
         </div>
     )
